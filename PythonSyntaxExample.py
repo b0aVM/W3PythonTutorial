2a8a847af1991c6bf5b4624956ba219dd616a0e7
b0aVM <b0a.vlmo@gmail.com>
2024-10-02 15:02:56 +0200

    PythonSyntaxExample

---
+++ PythonSyntaxExample.py
@@ -0,0 +1,5 @@
+if 5 > 2:
+    print("five is greater then two")
+    # Python will give you an error if you skip the indentation
+    # so it need to be some spaces
+    # The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
