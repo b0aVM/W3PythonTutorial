08b4d87cee38af5db090aad4cbc7568c6d3f9c9f
b0aVM <b0a.vlmo@gmail.com>
2024-10-02 15:55:24 +0200

    Variables

---
+++ PythonVariables/PythonVariables.py
@@ -0,0 +1,21 @@
+#Creating Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+y = "Sally" # x is now of type str
+print(x)
+
+#Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#Get the Type
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
